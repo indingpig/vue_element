0e05b2008eab370870b7b3d992be0269a1bbda5f
Sheldon <indoing@gmail.com>
2018-03-08 13:51:38 +0800

    修改登录界面切换壁纸按钮的样式

--- src/public/view/login.vue
+++ src/public/view/login.vue
@@ -2,16 +2,17 @@
     <div class="login-wrap">
         <div class="login-wrap-background" :style="{background:'url('+ imgUrl +')', opacity: opacityNum}">
         </div>
-        <div class="login">
-            <button @click="previous">Previous image</button>
-            <button @click="next">Next image</button>
+        <div class="button-group">
+            <div>
+                <a href="javasctipt:void(0)" @click="previous" class="button-previous"></a>
+                <a href="javasctipt:void(0)" @click="next" class="button-next"></a>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
     import axios from 'axios';
-    import backImage from './pic2460_017.jpg'
     export default {
         data () {
             return {
@@ -39,7 +40,7 @@
                     console.log(error)
                 })
             },
-            previous() {
+            previous(evt) {
                 this.opacityNum = 0;
                 this.day += 1;
                 this.getImgData();
@@ -70,8 +71,48 @@
         bottom: 0;
         transition: opacity 0.5s;
     }
-    .login {
-        position: relative;
+    .button-group {
+        width: 100%;
+        height: 43px;
+        position: absolute;
         z-index: 1;
+        bottom: 10px;
+        overflow: hidden;
+    }
+    .button-group > diV {
+        float: right;
+        margin-right: 5%;
+    }
+    .button-previous, .button-next{
+        display: inline-block;
+        width: 40px;
+        height: 40px;
+        background: #666;
+        text-decoration: none;
+        transition: background .5s;
+        opacity: .5;
+        position: relative;
+    }
+    .button-previous:hover, .button-next:hover {
+        background: #333;
+    }
+    .button-previous::after, .button-previous::before,.button-next::after, .button-next::before {
+        position:absolute;
+        width: 14px;
+        border: 1px solid #fff;
+        content: '';
+        transform: rotateZ(-45deg);
+        top: 14px;
+        left: 12px;
+    }
+    .button-previous::after, .button-next::after {
+        transform: rotateZ(45deg);
+        top: 25px;
+    }
+    .button-next::before {
+        transform: rotate(45deg);
+    }
+    .button-next::after {
+        transform: rotate(-45deg)
     }
 </style>
