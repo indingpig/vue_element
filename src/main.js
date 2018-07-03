cd01946f968b973cb93a4f5f217c4c5599a952b8
Sheldon <indoing@gmail.com>
2018-07-03 15:22:23 +0800

    增加webix

--- src/main.js
+++ src/main.js
@@ -6,13 +6,18 @@ import './public/css/index.css'
 // 引入element-ui
 import ElementUi from 'element-ui';
 // 引入underscore 
-import underscore from 'vue-underscore'
+import underscore from 'vue-underscore';
+// webix UI
+import './webix/webix.css';
+import webix from './webix/webix';
+import vueWebix from 'vue-webix';
 // 引入element-ui样式;
 import 'element-ui/lib/theme-chalk/index.css'
 import App from './App.vue';
 
 Vue.use(ElementUi);
 Vue.use(underscore);
+// Vue.use(webix);
 
 new Vue({
   el: '#app',
--- src/public/view/home/addShop.vue
+++ src/public/view/home/addShop.vue
@@ -1,23 +1,34 @@
 <template>
   <div>
       {{msg}}
-      <div @click="test">
-          点我试试
+      <div>
+          <webix-ui :config='ui'/>
       </div>
   </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                msg: '增加店铺'
-            }
-        },
-        methods: {
-            test(e){
-                e.preventDefault();
-            }
-        }
+export default {
+  data() {
+    return {
+      msg: "增加店铺",
+      ui: {
+        cols: [
+          {
+            view: "calendar"
+          },
+          {
+            view: "list",
+            select: true,
+          }
+        ]
+      }
+    };
+  },
+  methods: {
+    test(e) {
+      e.preventDefault();
     }
+  }
+};
 </script>
